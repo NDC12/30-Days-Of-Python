8035ac24ddfdb2f4b03f4bed7bfe9d597a79c43e
NDC12 <noahdouglasc@gmail.com>
2025-12-22 23:37:22 -0500

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,13 @@
-
+# Day 2: 30 Days of python programming
+first_name = 'LEbron'
+last_name = 'James'
+full_name = 'your mom'
+country = 'Jamaica'
+city = 'Kingston'
+age = '14'
+year = '2025'
+is_married = 'Jerome'
+is_true = 'LEbron is the GOAT'
+is_light_on = 'yes'
+dad, mom, brother, sister = 'papa', 'maman', 'frerot', 'soeur'
+type(first_name)
